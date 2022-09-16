0ccd41d7c348612fda1a5edc82c45408f5921a7a
LizaMatiash <liza.mymailbox@gmail.com>
2022-09-16 16:24:21 +0300

    Question acceptance nested atributes file

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'devise'
 
 gem 'jquery-rails'
 
+gem 'carrierwave'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
---
+++ spec/features/add_files_to_question_spec.rb
@@ -0,0 +1,27 @@
+require_relative 'feature_helper'
+
+feature 'Add files to question', %(
+  In order to illustrate my problem
+  As an aurhor of question
+  I want to add file
+) do
+  given(:user) { create(:user) }
+
+  background do
+    sign_in(user)
+    visit new_question_path
+  end
+
+  scenario 'Authenticated user creates question' do
+
+    fill_in 'Title', with: 'Test title'
+    fill_in 'Body', with: 'Test body'
+    attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+
+    click_on 'Create'
+
+    expect(page).to have_link 'rails_helper.rb'
+  end
+
+
+end
