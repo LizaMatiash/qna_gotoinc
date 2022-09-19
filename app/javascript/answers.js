a41fa7ee33a145ac7a1e47c578959adae9316d4b
LizaMatiash <liza.mymailbox@gmail.com>
2022-09-19 10:35:56 +0300

    Unlimited files checked

--- app/javascript/answers.js
+++ app/javascript/answers.js
@@ -9,12 +9,21 @@ $(document).on('turbolinks:load',function(){
   });
   $('.answers').on('click', '.save-answer-link', function(e){
    var answerId = $(this).data('answerId');
-   console.log(answerId);
    $('div.answer-' + answerId).hide();
+  })
+  $('.answers').on('click', '.destroy-file', function(e){
+   var fileId = $(this).data('fileId');
+   $(this).hide();
+   $('div.file-' + fileId).hide();
   })
   $('.edit-question').on('click', '.edit-question-link', function(e){
     e.preventDefault();
     $(this).hide();
     $('.edit-question form').show();
+  })
+  $('.edit-question').on('click', '.destroy-file', function(e){
+   var fileId = $(this).data('fileId');
+   $(this).hide();
+   $('div.file-' + fileId).hide();
   })
 });
